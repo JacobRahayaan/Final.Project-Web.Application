e166622fce7e82c7019f836749f0fe6e4d85da33
jacob <rahayaanjacob@gmail.com>
2024-05-23 23:30:56 +0700

    update lagi

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+var swiper = new swiper(".home-slider",{
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
